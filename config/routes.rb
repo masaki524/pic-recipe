0674338d4fad16704e7c8099b462b1e11ce5768c
masaki524 <d.g.pda0524@gmail.com>
2020-12-18 13:07:01 +0900

    検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'picrecipes/index'
   devise_for :users
   root to: "home#index"
   resources :users
   resources :recipes
+  get 'picrecipes/search'
   resources :picrecipes
 end
